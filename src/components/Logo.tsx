4f014c19b4d8951cb14ea48c61167313d50b39d4
Prajwal <prajwal@Prajwals-MacBook-Air.local>
2026-09-25 10:59:06 +0530

    Update logo to new PNG and add white background

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,11 @@
 export function Logo({ className = "h-16 w-auto", title = "Thrive Pain Clinic, Wellness & More" }) {
   return (
     <img
-      src="https://res.cloudinary.com/dizaawlyl/image/upload/v1790312385/WhatsApp_Image_2026-09-21_at_12.49.31_zt2me6.jpg"
+      src="https://res.cloudinary.com/dizaawlyl/image/upload/v1790313841/Thrive_PNG_elut6a.png"
       alt={title}
       width={1920}
       height={878}
-      className={`object-contain transition-all duration-200 ${className}`}
+      className={`object-contain bg-white transition-all duration-200 ${className}`}
     />
   );
 }
