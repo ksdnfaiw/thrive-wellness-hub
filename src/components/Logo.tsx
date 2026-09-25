0c29c9a7fd96ad7da576287c0a3ea398f6c2e390
Prajwal <prajwal@Prajwals-MacBook-Air.local>
2026-09-25 10:40:06 +0530

    Update logo URL to Cloudinary image

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,7 @@
-import logoAsset from "@/assets/thrive-logo.webp.asset.json";
-
 export function Logo({ className = "h-16 w-auto", title = "Thrive Pain Clinic, Wellness & More" }) {
   return (
     <img
-      src={logoAsset.url}
+      src="https://res.cloudinary.com/dizaawlyl/image/upload/v1790312385/WhatsApp_Image_2026-09-21_at_12.49.31_zt2me6.jpg"
       alt={title}
       width={1920}
       height={878}
